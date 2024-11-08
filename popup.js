e91adcf089277fb9d1203d9cb243cbb2ce2c617f
David Mejia <david7mejia@gmail.com>
2024-11-08 15:25:36 -0500

    add real time timer to popup

--- popup.js
+++ popup.js
@@ -1,8 +1,20 @@
-const timeEl = document.getElementById("time");
-const currentTime = new Date().toLocaleTimeString();
-const nameEl = document.getElementById("name");
-timeEl.textContent = `Time is ${currentTime}`;
+const timeElement = document.getElementById("time");
+const nameElement = document.getElementById("name");
+const timerElement = document.getElementById("timer");
 
-chrome.storage.sync.get(["name"], result => {
-  nameEl.textContent = `Hello, ${result.name}`;
+const updateTimeElements = () => {
+  chrome.storage.local.get(["timer"], res => {
+    const time = res.timer ?? 0;
+    timerElement.textContent = `The timer is at ${time} seconds`;
+  });
+  const currentTime = new Date().toLocaleTimeString();
+  timeElement.textContent = `Time is ${currentTime}`;
+};
+
+//the reason we call it first is because the interval will wait 1 second after we have opened the popup to start so here we run it once and then run the interval
+updateTimeElements();
+setInterval(updateTimeElements, 1000);
+chrome.storage.sync.get(["name"], res => {
+  const name = res.name ?? "who dis?";
+  nameElement.textContent = `Hello, ${name}`;
 });
